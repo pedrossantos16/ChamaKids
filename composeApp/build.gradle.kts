d265fb123a12393208a1ee4c036e43ff5ad7d471
Pedro Santos <pedroguimaraes147@gmail.com>
2026-09-03 16:22:42 -0300

    Migração para KMP e Setup de Build iOS

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -75,5 +75,8 @@ room {
 }
 
 dependencies {
-    add("kspCommonMain", libs.androidx.room.compiler)
+    add("kspAndroid", libs.androidx.room.compiler)
+    add("kspIosArm64", libs.androidx.room.compiler)
+    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
+    add("kspIosX64", libs.androidx.room.compiler)
 }
