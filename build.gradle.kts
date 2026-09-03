04377eee9e2edb2f62b0c6a256494ccbe788b41e
Pedro Santos <pedroguimaraes147@gmail.com>
2026-09-03 17:13:06 -0300

    Teste de migração

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
+// Configuração de Plugins Centralizada (Usando IDs diretos para estabilidade no CI)
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.1.0" apply false
     id("org.jetbrains.compose") version "1.7.3" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
