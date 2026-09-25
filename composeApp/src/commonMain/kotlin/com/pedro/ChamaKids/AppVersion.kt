73857260682c8933b42952fef76e2ac8a5d16173
Pedro Santos <pedroguimaraes147@gmail.com>
2026-09-25 16:41:17 -0300

    ATUALIZAÇÃO DOS CARDS DO HISTÓRICO EM TEMPO REAL
    CORREÇÃO DOS CALENDARIOS DA CHAMADA E ESTRELAS

--- composeApp/src/commonMain/kotlin/com/pedro/ChamaKids/AppVersion.kt
+++ composeApp/src/commonMain/kotlin/com/pedro/ChamaKids/AppVersion.kt
@@ -1,6 +1,6 @@
 package com.pedro.ChamaKids
 
 object AppVersion {
-    const val VERSION_NAME = "1.1.2"
-    const val VERSION_CODE = 12
+    const val VERSION_NAME = "1.1.3"
+    const val VERSION_CODE = 13
 }
